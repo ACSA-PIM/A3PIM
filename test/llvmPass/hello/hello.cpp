93b895b715b2168952e63930546db470f4007918
shaojiemike <943648187@qq.com>
2023-05-22 19:26:02 +0800

    test: llvm pass using cmake

---
+++ test/llvmPass/hello/hello.cpp
@@ -0,0 +1,32 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+using namespace llvm;
+
+namespace {
+ // Important
+  struct Hello : public FunctionPass {
+    static char ID;
+    Hello() : FunctionPass(ID) {}
+ // Important
+    bool runOnFunction(Function &F) override {
+      errs() << "Hello: ";
+      errs().write_escaped(F.getName()) << '\n';
+      return false;
+    }
+  };
+}
+
+char Hello::ID = 0;
+
+// Important:Register for opt
+static RegisterPass<Hello> X("hello", "Hello World Pass");
+
+// Important:Register for clang
+static RegisterStandardPasses Y(PassManagerBuilder::EP_EarlyAsPossible,
+  [](const PassManagerBuilder &Builder, legacy::PassManagerBase &PM) {
+    PM.add(new Hello());
+  });
